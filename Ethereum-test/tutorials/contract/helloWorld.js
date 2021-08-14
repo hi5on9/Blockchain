3f1daf6d7398e60cdb033dfc4daf0d670c140014
hi5on9 <0rwt0yqi@naver.com>
2021-08-14 23:48:11 +0900

    솔리디티 smc 실습

---
+++ Ethereum-test/tutorials/contract/helloWorld.js
@@ -0,0 +1,42 @@
+const address = "0x73d8e60a70106498B8d35B43dc8eF639D0392B9c";
+const abi = [
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "newMessage",
+        "type": "string"
+      }
+    ],
+    "name": "update",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "initMessage",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [],
+    "name": "message",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
+
+module.exports = { address, abi };
